f899d935926e76d0b0bb6c6d944abe3645d0dbe9
Mysak Alexander <reactionnnnn@gmail.com>
2018-12-06 00:07:27 +0200

    Add files via upload

---
+++ TriangleFinished/CVehicle.java
@@ -0,0 +1,39 @@
+package hw01;
+
+public abstract class CVehicle {
+    private int price;
+    private int speed;
+    private int releaseYear;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    @Override
+    public String toString() {
+        return "price=" + price +
+                ", speed=" + speed +
+                ", releaseYear=" + releaseYear +
+                '}';
+    }
+}
