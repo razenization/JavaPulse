785de036532c9d716bd0ec36be7067bf46abac78
Alex Mysak <reactionnnnn@gmail.com>
2018-12-06 00:50:52 +0200

    git pull

---
+++ CCar.java
@@ -0,0 +1,12 @@
+package hw01;
+
+public class CCar extends CVehicle {
+    public CCar() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "CCar{" + super.toString();
+    }
+}
